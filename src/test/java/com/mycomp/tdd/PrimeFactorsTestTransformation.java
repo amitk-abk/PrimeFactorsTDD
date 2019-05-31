f39f0c428f1275fd149a5fecf61a5a2a2bc4459b
Amit B Karande <amit.b.karande@boeing.com>
2019-05-31 10:07:17 +0530

    Refactored by moving split flow transformation for n > 1 outside inner loop transformed in previous step

--- src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
+++ src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
@@ -32,9 +32,9 @@ public class PrimeFactorsTestTransformation {
                 factors.add(2);
                 n /= 2;
             }
-            if (n > 1)
-                factors.add(n);
         }
+        if (n > 1)
+            factors.add(n);
         return factors;
     }
 }
