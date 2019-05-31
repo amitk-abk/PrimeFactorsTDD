03f08f11578c8bef21f71c20fd87b9c51ce53d90
Amit B Karande <amit.b.karande@boeing.com>
2019-05-31 10:18:40 +0530

    Added duplicated code specific to test case scenario to pass the test case; test passes

--- src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
+++ src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
@@ -32,9 +32,12 @@ public class PrimeFactorsTestTransformation {
 
     private List<Integer> primeFactorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        if (n > 1)
+        if (n > 1) {
             for (; n % 2 == 0; n /= 2)
                 factors.add(2);
+            for (; n % 3 == 0; n /= 3)
+                factors.add(3);
+        }
         if (n > 1)
             factors.add(n);
         return factors;
