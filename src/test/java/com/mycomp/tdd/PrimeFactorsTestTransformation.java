d9ed839456f1be4de33ef27af85c1ffaa524c169
Amit B Karande <amit.b.karande@boeing.com>
2019-05-31 10:05:16 +0530

    Added split flow transformation to add 2 as factor if n is divisible by 2; test passes

--- src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
+++ src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
@@ -27,8 +27,14 @@ public class PrimeFactorsTestTransformation {
 
     private List<Integer> primeFactorsOf(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
-        if (n > 1)
-            factors.add(n);
+        if (n > 1) {
+            if (n % 2 == 0) {
+                factors.add(2);
+                n /= 2;
+            }
+            if (n > 1)
+                factors.add(n);
+        }
         return factors;
     }
 }
