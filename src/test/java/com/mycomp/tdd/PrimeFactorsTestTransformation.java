00a7ba943547f860bc8240222cc1a2b48ede447d
Amit B Karande <amit.b.karande@boeing.com>
2019-05-31 09:50:16 +0530

    Initial commit : Code structure in place for transformations with prime factors code, test case failing

---
+++ src/test/java/com/mycomp/tdd/PrimeFactorsTestTransformation.java
@@ -0,0 +1,28 @@
+package com.mycomp.tdd;
+
+import org.hamcrest.Matcher;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PrimeFactorsTestTransformation {
+
+    private Matcher<? super List<Integer>> isListOf(Integer... ints) {
+        return is(asList(ints));
+    }
+
+    @Test
+    public void factors() throws Exception {
+        assertThat(primeFactorsOf(1), isListOf());
+    }
+
+
+    private List<Integer> primeFactorsOf(int n) {
+        return null;
+    }
+}
